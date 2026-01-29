58cf05566a2c6390647dc472346c3e94171a99c4
Chenxuan Li <rndm2830@leeds.ac.uk>
2026-01-29 16:01:04 +0000

    1.29（1）

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,7 +1,7 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
-SELECT Department.DepartmentName, COUNT(Course.EnrolmentId) AS TotalEnrolments
+SELECT Department.DepartmentName, COUNT(Enrolment.EnrolmentId) AS TotalEnrolments
 FROM Department 
     JOIN Course ON Department.DepartmentId = Course.DepartmentId
     JOIN Enrolment ON Course.CourseId = Enrolment.CourseId
